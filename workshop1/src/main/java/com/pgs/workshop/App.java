1c5ba5d44bfaec402183c2ae3f04618ae2bc0396
Lukasz Kuzio <lkuzio@gmail.com>
2017-03-24 20:11:50 +0100

    git example1

---
+++ workshop1/src/main/java/com/pgs/workshop/App.java
@@ -0,0 +1,10 @@
+package com.pgs.workshop;
+
+/**
+ * Created by Luke on 2017-03-24.
+ */
+public class App {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+}
