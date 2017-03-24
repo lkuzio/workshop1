0c855972a5c222c595156a017cb269427567713a
Lukasz Kuzio <lkuzio@gmail.com>
2017-03-24 21:02:43 +0100

    logback

--- workshop1/src/main/java/com/pgs/workshop/App.java
+++ workshop1/src/main/java/com/pgs/workshop/App.java
@@ -1,10 +1,22 @@
 package com.pgs.workshop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by Luke on 2017-03-24.
  */
 public class App {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args){
         System.out.println("Hello world!");
+
+        LOGGER.error("Error");
+        LOGGER.warn("Warn");
+        LOGGER.info("Info");
+        LOGGER.debug("debug");
+        LOGGER.trace("trace");
     }
 }
