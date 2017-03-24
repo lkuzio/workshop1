72d8de664863f5372873694a9ece604f3edec853
Lukasz Kuzio <lkuzio@gmail.com>
2017-03-24 22:20:42 +0100

    spring boot

--- workshop1/src/main/java/com/pgs/workshop/App.java
+++ workshop1/src/main/java/com/pgs/workshop/App.java
@@ -2,15 +2,28 @@ package com.pgs.workshop;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 /**
  * Created by Luke on 2017-03-24.
  */
+@SpringBootApplication
+@RestController
 public class App {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        SpringApplication.run(App.class);
+
         System.out.println("Hello world!");
 
         LOGGER.error("Error");
@@ -18,5 +31,18 @@ public class App {
         LOGGER.info("Info");
         LOGGER.debug("debug");
         LOGGER.trace("trace");
+    }
+
+
+    @RequestMapping(path = "/time")
+    public String getCurrentTime() {
+        return LocalDateTime.now().toString();
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return (container -> {
+            container.setPort(8989);
+        });
     }
 }
